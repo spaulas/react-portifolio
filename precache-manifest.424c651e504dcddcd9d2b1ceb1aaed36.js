524ddfc364da9c4199acf111874187763f84bd31
PSantosWave <s.paula@ua.pt>
2020-08-14 11:50:40 +0100

    Updates

---
+++ precache-manifest.424c651e504dcddcd9d2b1ceb1aaed36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ee4763b2f674e0dc212146abec4115",
+    "url": "/react-portifolio/index.html"
+  },
+  {
+    "revision": "82f1929061a4d0abd942",
+    "url": "/react-portifolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "41a411a14295dbc30eae",
+    "url": "/react-portifolio/static/js/2.a16978c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portifolio/static/js/2.a16978c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82f1929061a4d0abd942",
+    "url": "/react-portifolio/static/js/main.5324bc11.chunk.js"
+  },
+  {
+    "revision": "eaa7a26c6ddf0f0e81b2",
+    "url": "/react-portifolio/static/js/runtime-main.35090879.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portifolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
