2ac7d1b65e355c94dce11ce92ee232cdb7769230
PSantosWave <s.paula@ua.pt>
2020-08-24 13:50:08 +0100

    Updates

---
+++ precache-manifest.78318ae0108e622956d3bbca8ba97af5.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d6a251f8c11c8a98aeb88bc64fb767",
+    "url": "/react-portifolio/index.html"
+  },
+  {
+    "revision": "9808da77e104b51e509b",
+    "url": "/react-portifolio/static/js/0.b3a4fb68.chunk.js"
+  },
+  {
+    "revision": "2f25abf9e2410efbb2d2",
+    "url": "/react-portifolio/static/js/1.493dd011.chunk.js"
+  },
+  {
+    "revision": "660134c65eaa72491f05",
+    "url": "/react-portifolio/static/js/10.bd38bab7.chunk.js"
+  },
+  {
+    "revision": "10d3289c8e81a4c1e060",
+    "url": "/react-portifolio/static/js/11.1cd09a41.chunk.js"
+  },
+  {
+    "revision": "5210ca4737f8d1e870c0",
+    "url": "/react-portifolio/static/js/12.742b51a9.chunk.js"
+  },
+  {
+    "revision": "412c8c7cd832be2ff567",
+    "url": "/react-portifolio/static/js/13.f20db497.chunk.js"
+  },
+  {
+    "revision": "1387eee72031887752f7",
+    "url": "/react-portifolio/static/js/14.76445b98.chunk.js"
+  },
+  {
+    "revision": "25fff7d8a0a832120a78",
+    "url": "/react-portifolio/static/js/15.54b02c18.chunk.js"
+  },
+  {
+    "revision": "c731ca2e50eb7f1d703d",
+    "url": "/react-portifolio/static/js/16.1e909fef.chunk.js"
+  },
+  {
+    "revision": "f37776440ecfc4cd39e9",
+    "url": "/react-portifolio/static/js/4.bc3a9425.chunk.js"
+  },
+  {
+    "revision": "78ae2d820a9dd00083ad",
+    "url": "/react-portifolio/static/js/5.a3ba0d5a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portifolio/static/js/5.a3ba0d5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a13659a6de79c7d39f6",
+    "url": "/react-portifolio/static/js/6.ea79f8de.chunk.js"
+  },
+  {
+    "revision": "8634329beab6275d711a",
+    "url": "/react-portifolio/static/js/7.f4c4738c.chunk.js"
+  },
+  {
+    "revision": "9c5dccda2ce13038cf4d",
+    "url": "/react-portifolio/static/js/8.17f5ff3e.chunk.js"
+  },
+  {
+    "revision": "b4426ed433c3f3cb422c",
+    "url": "/react-portifolio/static/js/9.ec9a283f.chunk.js"
+  },
+  {
+    "revision": "fed94b2f48bccb9a6b30",
+    "url": "/react-portifolio/static/js/main.832d901c.chunk.js"
+  },
+  {
+    "revision": "1268b6238ae3d74fcb6a",
+    "url": "/react-portifolio/static/js/runtime-main.eeabc38e.js"
+  },
+  {
+    "revision": "a4b068ee8a8bdb4d976648992bb1db90",
+    "url": "/react-portifolio/static/media/Manrope-ExtraLight.a4b068ee.ttf"
+  },
+  {
+    "revision": "b29324c45ced02ca418e097ae8f52c77",
+    "url": "/react-portifolio/static/media/antd.b29324c4.svg"
+  },
+  {
+    "revision": "ae427edb9414023b7458031f6c5b5bb5",
+    "url": "/react-portifolio/static/media/arrowDown.ae427edb.svg"
+  },
+  {
+    "revision": "0cdf6b89bd0041c96577d2e40dccae18",
+    "url": "/react-portifolio/static/media/css.0cdf6b89.png"
+  },
+  {
+    "revision": "aec33a6cf762719b9ee757d2ce4aec2f",
+    "url": "/react-portifolio/static/media/developer.aec33a6c.png"
+  },
+  {
+    "revision": "6738fee9c427243ef421320bddce525d",
+    "url": "/react-portifolio/static/media/firebase.6738fee9.png"
+  },
+  {
+    "revision": "1f33e75d7b7df995b1609f8573ac8c9b",
+    "url": "/react-portifolio/static/media/github.1f33e75d.svg"
+  },
+  {
+    "revision": "24c27a3c0ed52cc7888f514f586b15f4",
+    "url": "/react-portifolio/static/media/lightMode.24c27a3c.svg"
+  },
+  {
+    "revision": "3c8afea5ee97ad99924808a2ab5fec10",
+    "url": "/react-portifolio/static/media/linkedin.3c8afea5.svg"
+  },
+  {
+    "revision": "4b49a1db9319f549116fca373e121348",
+    "url": "/react-portifolio/static/media/mySQL.4b49a1db.png"
+  },
+  {
+    "revision": "71f926988344cf8ef72e2c3ae2431749",
+    "url": "/react-portifolio/static/media/phpSocialNetwork_main.71f92698.png"
+  },
+  {
+    "revision": "d71eafb6030f99f84b675debbd466074",
+    "url": "/react-portifolio/static/media/phpSocialNetwork_side.d71eafb6.png"
+  },
+  {
+    "revision": "c6fef9b839814bbbd30a810cab5e0827",
+    "url": "/react-portifolio/static/media/reactSolitaire_main.c6fef9b8.png"
+  },
+  {
+    "revision": "5253f8c7ac498f8b8599db3f3aadb90e",
+    "url": "/react-portifolio/static/media/reactSolitaire_side.5253f8c7.png"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/react-portifolio/static/media/redux.aa0a3376.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portifolio/precache-manifest.2db7fb4a08da93adbb6cc6445fda9c10.js"
+  "/react-portifolio/precache-manifest.78318ae0108e622956d3bbca8ba97af5.js"
 );
 
 self.addEventListener('message', (event) => {
