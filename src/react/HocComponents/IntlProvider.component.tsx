05a29d1320a22ec87dfb9c5a4bbbaab5f6c59f20
PSantosWave <s.paula@ua.pt>
2020-09-01 10:45:25 +0100

    clean up

--- src/react/HocComponents/IntlProvider.component.tsx
+++ src/react/HocComponents/IntlProvider.component.tsx
@@ -1,10 +1,10 @@
 import { ExplicitAny, RootReducerState } from "../../global";
 import React, { PropsWithChildren, memo } from "react";
 import { IntlProvider } from "react-intl";
-import deDEIntl from "../Languages/Compiled/de-DE.json";
-import enUSIntl from "../Languages/Compiled/en-US.json";
-import esESIntl from "../Languages/Compiled/es-ES.json";
-import ptBRIntl from "../Languages/Compiled/pt-BR.json";
+import deDEIntl from "../Languages/de-DE.json";
+import enUSIntl from "../Languages/en-US.json";
+import esESIntl from "../Languages/es-ES.json";
+import ptBRIntl from "../Languages/pt-BR.json";
 import { useSelector } from "react-redux";
 
 function TranslatorIntlProvider({ children }: PropsWithChildren<{}>) {
